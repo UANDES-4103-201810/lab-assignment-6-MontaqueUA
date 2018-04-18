abdacdf6b8f068abfbec7c9cde0bf42635eda7a1
monty <iacarril@miuandes.cl>
2018-04-17 23:51:48 -0300

    some login stuff missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,10 @@ class ApplicationController < ActionController::Base
   def current_user
 	  @_current_user ||= session[:current_user] &&
         User.find_by(id: session[:current_user])
+
   end
 
   def is_user_logged_in?
-	#complete this method
   	logged_in = false
 	if logged_in then true else redirect_to root_path end 
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,35 @@
 class SessionsController < ApplicationController
 	def new
 	end
-
 	def create
-		user = User.find_by(name: params[:name])
-		if user and user.authenticate[:password]
+		user = User.find_by email: session_params[:email]
+		if authenticate(user.email, session_params[:password])
 			sessions[:user_id] = user.id
-			redirect_to admin_url
+			flash[:notice] = "Login complete"
+			set_cookie
+			redirect_to show
     else
-      redirect_to log_in_url, alert:'User or password is incorrect'
+      redirect_to root_path, alert:'User or password is incorrect'
     end
 	end
 
 	def destroy
-		#complete this method
+		@_current_user = session[:current_user_id] = nil
+		flash[:notice] = "You have successfully logged out."
+		redirect_to root_url
+	end
+	def authenticate (user_email, password)
+		user = User.find_by email:user_email
+		if password == user.password
+			return true
+		else
+			return false
+		end
+	end
+	def set_cookie
+		cookies.encrypted[:expiration_date] = Time.now + 40
+	end
+	def session_params
+		params.require(:session).permit(:email, :password)
 	end
 end
